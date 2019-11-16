18d0e37bb6c3800450dddccbfe7d9e2d3ceb49b1
lhh092589 <hyr18051362606@sina.com>
2019-11-16 18:58:55 +0800

    Initial commit

---
+++ src/main/java/com/hyr/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.hyr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+    @RequestMapping("/toLogin.do")
+    public String toLogin(){
+        return "login";
+    }
+}
